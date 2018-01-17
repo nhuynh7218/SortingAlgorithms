8f56b2147f7df92852ccaed11af1bfb14d8421a0
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-17 09:22:47 -0500

    Added swap method

---
+++ SortingLab/src/SortingStuff.java
@@ -0,0 +1,11 @@
+
+public class SortingStuff {
+
+public static void swapInt(int[]array, int index1, int index2)
+{
+	int x = array[index1];
+	array[index1] = array[index2];
+	array[index2] = x;
+}
+
+}
