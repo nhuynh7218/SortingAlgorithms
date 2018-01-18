5b5ba93fb1f7cfb6f82dc9f9915a5863d898bb80
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-18 09:28:57 -0500

    erg

--- SortingLab/src/SortingAlgorithms.java
+++ SortingLab/src/SortingAlgorithms.java
@@ -1,4 +1,19 @@
 
 public class SortingAlgorithms {
-
+	public static void bubbleSort(String [] list1)
+	{
+		int swapCount = 0;
+		while(swapCount > 0)
+		{
+			for(int i = 0; i<= list1.length-1;i++)
+			{
+				int var1 = list1[i].compareTo(list1[i+1]);
+				if (var1 > 0)
+				{
+					SwapMethods.swapString(list1, i, i+1);
+					swapCount--;
+				}
+			}
+		}
+	}
 }
