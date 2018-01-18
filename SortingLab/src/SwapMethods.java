baa5583bc9b634b7e919fe49151fd5e3613705db
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-18 09:02:49 -0500

    finished swap methods

--- SortingLab/src/SwapMethods.java
+++ SortingLab/src/SwapMethods.java
@@ -8,5 +8,17 @@ public static void swapInt(int[]array, int index1, int index2)
 	array[index2] = x;
 }
 
+public static void swapString(String[]array, int index1, int index2)
+{
+	String hold = array[index1];
+	array[index1].equals(array[index2]);
+	array[index2].equals(hold);
+}
 
+public static void swapDouble(double[]array, int index1, int index2)
+{
+	double x = array[index1];
+	array[index1] = array[index2];
+	array[index2] = x;
+}
 }
