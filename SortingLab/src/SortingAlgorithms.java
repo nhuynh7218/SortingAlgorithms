fb2e6d137f07b512bd3fcabf710ea3be1b99e205
nickn <nickn@DESKTOP-B2VPUKN>
2018-01-19 00:18:15 -0500

    w5i7

--- SortingLab/src/SortingAlgorithms.java
+++ SortingLab/src/SortingAlgorithms.java
@@ -5,7 +5,8 @@ public class SortingAlgorithms {
 		int swapCount = 0;
 		while(swapCount > 0)
 		{
-			for(int i = 0; i<= list1.length-1;i++)
+			int len = list1.length;
+			for(int i = len; i >= 0; i++)
 			{
 				int var1 = list1[i].compareTo(list1[i+1]);
 				if (var1 > 0)
@@ -15,5 +16,17 @@ public class SortingAlgorithms {
 				}
 			}
 		}
+	}
+	
+	
+	public static void selectionSort(double[]list1)
+	{
+		
+	}
+	
+	
+	public static void insertionSort(int[]list1)
+	{
+		
 	}
 }
